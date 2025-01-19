3697d89437a59799d17ecbcab788cedf0d9c8a5b
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-01-19 23:24:29 +0700

    demo openzeppelin contract

--- How_to_create_this_project.md
+++ How_to_create_this_project.md
@@ -3,4 +3,4 @@
 ```npx hardhat init``` (typescript - yes - yes)
 ```git init```
 ```git remote add origin <git-repo>```
-
+```npm install @openzeppelin/contracts```
---
+++ contracts/Box.sol
@@ -0,0 +1,26 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+// Import Ownable from the OpenZeppelin Contracts library
+// document: https://docs.openzeppelin.com/contracts/5.x/
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
+    uint256 private _value;
+
+    event ValueChanged(uint256 value);
+
+    constructor() Ownable(msg.sender) {}
+
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 value) public onlyOwner {
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
